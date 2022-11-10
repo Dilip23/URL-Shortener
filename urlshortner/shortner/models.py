7baf95e8a991d57b7f3af3848da63b771389f555
Adapaka Dilip Krishna <30292638+Dilip23@users.noreply.github.com>
2022-11-10 18:17:42 +0530

    URL shortener perfectly working

---
+++ urlshortner/shortner/models.py
@@ -0,0 +1,7 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+class Url(models.Model):
+    link = models.CharField(max_length = 10000)
+    uuid = models.CharField(max_length = 10)
